c15004dd319928fd282cb5279afa30d878c4a961
namnx1 <namnx1@vietis.com.vn>
2022-06-02 15:34:10 +0700

    fix

--- lib/ui/pages/sign_up/sign_up_view.dart
+++ lib/ui/pages/sign_up/sign_up_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:sbsi/ui/pages/sign_up/page/verify_account.dart';
 
 import 'page/referral_page.dart';
 import 'sign_up_logic.dart';
@@ -18,7 +17,7 @@ class _SignUpPageState extends State<SignUpPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const VerifyAccount();
+    return const ReferralPage();
   }
 
   @override
